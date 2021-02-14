228002a40f276a7219d13ffb789f7441fd0041a9
morgmaestro <70662172+morgmaestro@users.noreply.github.com>
2021-02-14 09:46:26 -0800

    made beads work!

--- final.js
+++ final.js
@@ -1,11 +1,14 @@
+// counter only counts if it hits upperbound
 const beads = document.querySelectorAll(".bead");
 dragElement(document.querySelectorAll(".bead"));
-console.log(beads);
 
-beads.forEach(bead => dragElement(bead), console.log(window.event));
+beads.forEach((bead, beadIndex) => dragElement(bead, beadIndex));
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0;
+function dragElement(elmnt, currentBeadIndex) {
+  let pos1 = 0;
+  let pos2 = 0;
+  let lowerBound = 0;
+  let upperBound = 950;
   elmnt.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
@@ -17,6 +20,28 @@ function dragElement(elmnt) {
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
+
+    // lower bound is position of the adjacent child. 
+    // which bead are we dragging?
+    // which bead is next to the one we're dragging?
+    // (theCurrentBead's index - 1)
+    // (theCurrentBead's index + 1)
+    const lowerAdjacentBead = beads[currentBeadIndex - 1];
+    const upperAdjacentBead = beads[currentBeadIndex + 1];
+
+    if (upperAdjacentBead) {
+      upperBound = upperAdjacentBead.offsetLeft - 50;
+    } else {
+      upperBound = 950;
+    }
+
+    if (lowerAdjacentBead) {
+      lowerBound = lowerAdjacentBead.offsetLeft + 50;
+    } else {
+      lowerBound = 0;
+    }
+
+    console.log(lowerBound, upperBound);
   }
 
   function elementDrag(e) {
@@ -27,8 +52,12 @@ function dragElement(elmnt) {
     pos2 = e.clientX;
     // set the element's new position:
 
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    console.log(elmnt.style.left);
+    const newOffset = (elmnt.offsetLeft - pos1);
+    // 0 and 950 are our lower and upper bounds.
+    if (newOffset >= lowerBound && newOffset <= upperBound) {
+      elmnt.style.left = newOffset + "px";
+      console.log(elmnt.style.left);
+    }
   }
 
   function closeDragElement() {
